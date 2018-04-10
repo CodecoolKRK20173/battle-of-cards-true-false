9ff2b39147a117d5a3169ea906ab7a896525de7d
szymslow12 <szym.slow12@gmail.com>
2018-04-10 12:54:38 +0200

    add Stack with addCard/removeCard/toString

--- src/main/java/com/codecool/java/cheatCardGame/Stack.java
+++ src/main/java/com/codecool/java/cheatCardGame/Stack.java
@@ -1,6 +1,29 @@
-
 package com.codecool.java.cheatCardGame;
+
+import java.util.List;
+import java.util.ArrayList;
 
 class Stack implements Stackable {
 
+    List<Card> stackCards;
+
+    public Stack() {
+        this.stackCards = new ArrayList<>();
+    }
+
+
+    public void addCard(Card card) {
+        this.stackCards.add(card);
+    }
+
+
+    public void removeCard(Card card) {
+        this.stackCards.remove(card);
+    }
+
+
+    public String toString() {
+        String stack = "Cards on stack: " + this.stackCards.size();
+        return stack;
+    }
 }
