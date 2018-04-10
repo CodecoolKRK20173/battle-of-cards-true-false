c6b9c92365315f1efacd977ff42cbc62984e9b2f
szymslow12 <szym.slow12@gmail.com>
2018-04-10 11:15:35 +0200

    change in names macesNumber -> clubsNumber

--- src/main/java/com/codecool/java/cheatCardGame/Hand.java
+++ src/main/java/com/codecool/java/cheatCardGame/Hand.java
@@ -35,7 +35,7 @@ public class Hand implements Stackable {
         int[] cardsBySuit = calculateCardsBySuit();
         String hand = "Cards in hand: " + cardsBySuit[0] + "x H, " +
                     cardsBySuit[1] + "x D, " +
-                    cardsBySuit[2] + "x M, " +
+                    cardsBySuit[2] + "x C, " +
                     cardsBySuit[3] + "x S";
         return hand;
     }
@@ -44,20 +44,20 @@ public class Hand implements Stackable {
     private int[] calculateCardsBySuit() {
         int heartsNumber = 0;
         int diamondsNumber = 0;
-        int macesNumber = 0;
+        int clubsNumber = 0;
         int spadesNumber = 0;
         for (Card card: cardsList) {
             if (card.getSuit() == Card.Suit.HEARTS)
                 heartsNumber += 1;
             else if (card.getSuit() == Card.Suit.DIAMONDS)
                 diamondsNumber += 1;
-            else if (card.getSuit() == Card.Suit.MACES)
-                macesNumber += 1;
+            else if (card.getSuit() == Card.Suit.CLUBS)
+                clubsNumber += 1;
             else
                 spadesNumber += 1;
         }
         int[] cardsBySuit = {heartsNumber, diamondsNumber,
-                            macesNumber, spadesNumber};
+                            clubsNumber, spadesNumber};
         return cardsBySuit;
     }
 }
