7b2818f113a8e1eb2a03e65a776ea0e073db2767
szymslow12 <szym.slow12@gmail.com>
2018-04-10 13:08:57 +0200

    change addCard/removeCard takes parameter -> Card

--- src/main/java/com/codecool/java/cheatCardGame/Stackable.java
+++ src/main/java/com/codecool/java/cheatCardGame/Stackable.java
@@ -2,7 +2,7 @@
 package com.codecool.java.cheatCardGame;
 
 interface Stackable {
-    // public void addCard();
-    // public void removeCard();
-    // public String toString();
+    public void addCard(Card card);
+    public void removeCard(Card card);
+    public String toString();
 }
