c669618ac9d8ecf0659b8dc3d86a17d9e46ec882
szymslow12 <szym.slow12@gmail.com>
2018-04-11 12:29:15 +0200

    fix getSuitName

--- src/main/java/com/codecool/java/cheatCardGame/models/Suit.java
+++ src/main/java/com/codecool/java/cheatCardGame/models/Suit.java
@@ -19,8 +19,13 @@ package com.codecool.java.cheatCardGame.models;
         }
 
         public int getSuitName() {
-            //FIX:
-            return suitValue;
+            if (suitValue == 1)
+                return "hearts";
+            else if (suitValue == 2)
+                return "diamonds";
+            else if (suitValue == 3)
+                return "spades";
+            else
+                return "clubs";
         }
     }
-
