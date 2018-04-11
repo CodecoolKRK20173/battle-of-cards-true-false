941a8ac35b9199331ab5ba878e0a6b9bf7340885
szymslow12 <szym.slow12@gmail.com>
2018-04-11 12:28:52 +0200

    add import / change getPlayerName -> getName

--- src/main/java/com/codecool/java/cheatCardGame/View.java
+++ src/main/java/com/codecool/java/cheatCardGame/View.java
@@ -1,5 +1,6 @@
 package com.codecool.java.cheatCardGame;
 
+import com.codecool.java.cheatCardGame.models;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -24,7 +25,7 @@ public class View {
     public void showNumOfEnemyCards() {
         System.out.print("Enemy players cards: ");
         for (Player enemy: enemyPlayers) {
-            System.out.print(enemy.getPlayerName() + " -> " +
+            System.out.print(enemy.getName() + " -> " +
                             enemy.getHand().getNumOfCards()+ "x ");
         } System.out.println("");
     }
@@ -36,9 +37,9 @@ public class View {
 
 
     public void showAllPlayers() {
-        System.out.print(player.getPlayerName());
+        System.out.print(player.getName());
         for (Player enemy: enemyPlayers) {
-            System.out.print(" " + enemy.getPlayerName());
+            System.out.print(" " + enemy.getName());
         } System.out.println("");
     }
 
@@ -46,7 +47,7 @@ public class View {
     public void showEnemyMove(Player enemy) {
         String move1 = "Throw a card";
         String move2 = "Check a stack";
-        System.out.print("Player " + enemy.getPlayerName() + ": ");
+        System.out.print("Player " + enemy.getName() + ": ");
         if (enemy.getPlayerMove().equalsIgnoreCase(move1))
             System.out.print(move1);
         else
