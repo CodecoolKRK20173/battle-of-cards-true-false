0c2e7fafe01ba0a32d1ea3626d46c9d6a863d4e8
szymslow12 <szym.slow12@gmail.com>
2018-04-13 13:11:40 +0200

    change main invokes PlayerVsPlayer

--- src/main/java/com/codecool/java/cheatCardGame/Main.java
+++ src/main/java/com/codecool/java/cheatCardGame/Main.java
@@ -4,8 +4,9 @@ import com.codecool.java.cheatCardGame.controllers.*;
 
 public class Main {
     public static void main(String[] args) {
-            AppController app = new AppController();
-            app.run();
+            //AppController app = new AppController();
+            //app.run();
+            PlayerVsPlayer pvp = new PlayerVsPlayer();
 
         }
     }
