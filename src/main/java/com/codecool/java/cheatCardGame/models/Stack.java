efabf96aaa1af3561d22b91e0767c3dc3c719abc
szymslow12 <szym.slow12@gmail.com>
2018-04-13 13:10:52 +0200

    add getCardSymbol

--- src/main/java/com/codecool/java/cheatCardGame/models/Stack.java
+++ src/main/java/com/codecool/java/cheatCardGame/models/Stack.java
@@ -26,6 +26,23 @@ public class Stack implements Stackable {
 
     public String toString() {
         String stack = "Cards on stack: " + this.stackCards.size();
+        stack += " " + getCardSymbol();
         return stack;
+    }
+
+
+    private String getCardSymbol() {
+        Card topCard = stackCards.size() > 0 ? stackCards.get(stackCards.size() - 1): null;
+        Suit topSuit = topCard != null ? topCard.getSuit(): null;
+        if (topSuit == Suit.HEARTS)
+            return "\u001B[31m\u2665\u001B[0m";
+        else if (topSuit == Suit.DIAMONDS)
+            return "\u001B[31m\u2666\u001B[0m";
+        else if (topSuit == Suit.CLUBS)
+            return "\u001B[30m\u2663\u001B[0m";
+        else if (topSuit == Suit.SPADES)
+            return "\u001B[30m\u2660\u001B[0m";
+        else
+            return "";
     }
 }
